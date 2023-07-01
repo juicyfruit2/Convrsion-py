94817ea50f911580f74fcb2ff7c1a360fe4b9441
juicyfruit2 <134799673+juicyfruit2@users.noreply.github.com>
2023-07-01 15:22:30 +0200

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,18 @@
+num1 = 99.23
+print(99.23)
+num2 = 23
+print(3)
+num3 = 150
+print(150)
+string1 = "100"
+print(string1)
+
+# conversion
+Num1 = 99.23
+print(int(Num1))
+Num2 = 23
+print(float(Num2))
+Num3 = 150
+print(str(Num3))
+string1 = "100"
+print(int("100"))
